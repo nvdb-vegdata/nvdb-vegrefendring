fbb957d53d2b21f6da9f88c927a319d22e7e53aa
LtGlahn <jensen72@gmail.com>
2019-07-23 18:27:53 +0200

    prototype m historikk-i-punkt funker

--- kart_old/proxy2.php
+++
@@ -1,7 +0,0 @@
-﻿<?php function getData($lat, $lon) {
-    $url = 'https://jansimple.pythonanywhere.com/sjekkvegreferanser?lon='.$lon.'&lat='.$lat;	$output = file_get_contents( $url); 	return $output;
-}
-$data = getData($_GET['lat'], $_GET['lon']);
-print_r($data);
-
-?>
